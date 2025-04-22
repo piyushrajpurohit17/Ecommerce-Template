34648a6a187f5462685a2fe7f982de8ee2152b28
piyushrajpurohit17 <piyushrajpurohit1704@gmail.com>
2025-04-22 14:11:14 +0530

    Frontend setup

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+export default function App() {
+  return (
+    <>
+    
+    <BrowserRouter>
+
+      <Routes>
+
+        
+      </Routes>
+    
+    </BrowserRouter>
+   
+    
+    </>
+  )
+}
